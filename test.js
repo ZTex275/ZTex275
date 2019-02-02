b8ee694512d6ccc76cb1369f64a514f020e31873
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-02 10:42:29 +0300

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-var name = prompt("Укажите ваше имя")
-var date = prompt("Укажите год вашего рождения")
-var get_current_age = function(date) {
-return new Date().getFullYear() - date
-}
-alert (name+" "+get_current_age (date))
